2d034298b714f7e1a12e377d7830997a275bef27
markjoescott <m-scott@kellogg.northwestern.edu>
2014-05-07 14:00:16 -0500

    Began HW#6
    Corrected controllers, added square method

--- app/controllers/calculation_controller.rb
+++ app/controllers/calculation_controller.rb
@@ -1,12 +1,25 @@
 class CalculationController < ApplicationController
   def home
+
+    render 'home'
+
   end
 
-  def square_root
+  def sq
+    @sqr_number = params[:number].to_f
+    @sqr_answer = @sqr_number**2
+
+
+  end
+
+  def sqrt
     @the_number = params[:number].to_f
     @answer = Math.sqrt(@the_number)
   end
 
   def pmt
+
+    @payment == 1
+
   end
 end
